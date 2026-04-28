b1644eb1b71a8d263e5e8aa94c19c097c3c69289
cramerservices <cramerservicesllc@gmail.com>
2026-04-27 22:06:18 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/plans">
+    <BrowserRouter basename="/plans"> 
       <App />
     </BrowserRouter>
   </React.StrictMode>
